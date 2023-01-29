741c3617981634701451fe699bd20388e7025cc3
dahyeon405 <dahyeon405@naver.com>
2023-01-29 22:06:14 +0900

    230129-1

--- 자료 구조/스택과 큐/pg두큐합같게만들기.js
+++ 자료 구조/스택과 큐/pg두큐합같게만들기.js
@@ -6,32 +6,52 @@ function solution(queue1, queue2) {
   let halfsum = sum / 2;
 
   let halfsumSection = [];
+
+  let startIdx = 0;
+  let curSum = 0;
+
+  let notFound = false; // k가 끝까지 갔는데 못 찾았을 경우
   for (let i = 0; i < q.length; i++) {
-    let curSum = 0;
-    for (let k = i; k < q.length; k++) {
+    if (q[i] > halfsum) return -1;
+    if (notFound) break;
+    for (let k = startIdx; k < q.length; k++) {
       curSum += q[k];
+
+      if (curSum > halfsum) {
+        startIdx = k;
+        curSum = curSum - q[i] - q[k];
+        break;
+      }
       if (curSum === halfsum) {
         halfsumSection.push([i, k]);
+        curSum -= q[i];
+        startIdx = k + 1;
         break;
+      }
+      if (k === q.length - 1) {
+        notFound = true;
       }
     }
   }
 
   if (halfsumSection.length === 0) return -1;
+  const q2startIndex = queue1.length;
+  const queue1Length = queue1.length;
+  const queue2Length = queue2.length;
+
   function calculateCnt(section) {
     let [i, k] = section;
-    const q2startIndex = queue1.length;
-    if (k < q2startIndex) {
-      if (k === q2startIndex - 1) return i;
+    if (k < queue1Length) {
+      if (k === queue1Length - 1) return i;
       else {
-        return queue2.length + i;
+        return queue2Length + i;
       }
-    } else if (i < q2startIndex && k < q.length - 1) {
-      return i + k - q2startIndex + 1;
-    } else if (i >= q2startIndex) {
-      if (k === q.length - 1) return i - q2startIndex;
+    } else if (i < queue1Length && k < q.length - 1) {
+      return i + k - queue1Length + 1;
+    } else if (i >= queue1Length) {
+      if (k === q.length - 1) return i - queue1Length;
       else {
-        return k - q2startIndex + 1 + queue1.length + i - q2startIndex;
+        return k - queue1Length + 1 + i;
       }
     }
   }
@@ -41,4 +61,3 @@ function solution(queue1, queue2) {
 
   return count;
 }
-// 반 시간초과 ..
