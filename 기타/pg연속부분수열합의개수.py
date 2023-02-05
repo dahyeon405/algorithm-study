1287147c913a8b5e6f614492960a01dec410fcb3
dahyeon405 <dahyeon405@naver.com>
2023-02-05 11:00:29 +0900

    230205-2

--- 기타/pg연속부분수열합의개수.py
+++ 기타/pg연속부분수열합의개수.py
@@ -7,3 +7,18 @@ def solution(elements):
             resultSet.add(sum(q[k:k+i]))    
     
     return len(resultSet)
+
+# 좀 더 빠른 풀이
+def solution(elements):
+    answer = 0
+    
+    q = elements*2
+    resultSet = set()
+    
+    for k in range(len(elements)):
+        sum = 0
+        for i in range(1, len(elements)+1):
+            sum += q[k+i]
+            resultSet.add(sum)    
+    
+    return len(resultSet)
